688c880918b52185d9bb24e155c9c995c24d7328
J1mmyLau <liujiaming@zju.edu.cn>
2025-01-30 13:03:40 +0800

    fixed bugs

--- lab3/randomizedtest/AListNoResizing.java
+++ lab3/randomizedtest/AListNoResizing.java
@@ -1,5 +1,8 @@
 package randomizedtest;
 
+import edu.princeton.cs.algs4.StdRandom;
+import org.junit.Test;
+
 /** Array based list.
  *  @author Josh Hug
  */
@@ -51,5 +54,25 @@ public class AListNoResizing<Item> {
         items[size - 1] = null;
         size = size - 1;
         return x;
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+
+        int N = 500;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 2);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                System.out.println("size: " + size);
+            }
+        }
     }
 }
--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -7,6 +7,30 @@ import static org.junit.Assert.*;
 /**
  * Created by hug.
  */
+
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void randomizedmutiloperationTest(){
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> B = new BuggyAList<>();
+        for (int i = 0; i < 100000; i++) {
+            int operation = StdRandom.uniform(0, 4);
+            if (operation == 0) {
+                L.addLast(i);
+                B.addLast(i);
+            } else if (operation == 1) {
+                assertEquals(L.size(), B.size());
+            } else if (operation == 2) {
+                if (L.size() > 0) {
+                    assertEquals(L.removeLast(), B.removeLast());
+                }
+            } else {
+                if (L.size() > 0) {
+                    int index = StdRandom.uniform(0, L.size());
+                    assertEquals(L.get(index), B.get(index));
+                }
+            }
+        }
+    }
 }
