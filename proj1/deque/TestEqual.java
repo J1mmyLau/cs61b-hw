8600d483c6e5f26cc71d62e4c1433287e4a8dd97
J1mmyLau <liujiaming@zju.edu.cn>
2025-02-03 22:26:58 +0800

    hhh

---
+++ proj1/deque/TestEqual.java
@@ -0,0 +1,36 @@
+package deque;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestEqual {
+    @Test
+    public void testEqual() {
+        ArrayDeque<String> ad1 = new ArrayDeque<String>();
+        LinkedListDeque<String> ad2 = new LinkedListDeque<String>();
+
+        ad1.addFirst("front");
+        ad1.addLast("middle");
+        ad1.addLast("back");
+
+        ad2.addFirst("front");
+        ad2.addLast("middle");
+        ad2.addLast("back");
+
+        assertEquals(ad1, ad2);
+    }
+
+    @Test
+    public void DeepEqualTest() {
+        //very long deque
+        ArrayDeque<Integer> ad1 = new ArrayDeque<Integer>();
+        LinkedListDeque<Integer> ad2 = new LinkedListDeque<Integer>();
+        for (int i = 0; i < 1000; i++) {
+            ad1.addLast(i);
+            ad2.addLast(i);
+        }
+        assertEquals(ad1, ad2);
+
+    }
+}
