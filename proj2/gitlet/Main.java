971f79537b794f58e2b8cf569d43fea82e64a78d
J1mmyLau <liujiaming@zju.edu.cn>
2025-02-16 17:14:46 +0800

    fix add and find

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -27,9 +27,6 @@ public class Main {
                 break;
             }
             case "commit": {
-                for(int i = 2; i < args.length; i++) {
-                    args[1] += " " + args[i];
-                }
                 repo.commit(args[1], false);
                 break;
             }
